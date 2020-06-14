40678f86602ab669db3554813a807a56a4f86a82
luca <luca1230@gmail.com>
2020-06-13 22:18:57 -0700

    added zsh theme and plugins to the script

--- init.sh
+++ init.sh
@@ -4,11 +4,14 @@ ln -s ${DIR}/.vimrc ~/.vimrc
 ln -s ${DIR}/.zshrc ~/.zshrc
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git
+git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+mv jason.zsh-theme oh-my-zsh/custom/themes
 ln -s ${DIR}/.oh-my-zsh ~/.oh-my-zsh
 
-git clone https://github.com/preservim/nerdtree.git ./vim/bundle/nerdtree
-git clone https://github.com/ctrlpvim/ctrlp.vim.git ./vim/bundle/ctrlp
-git clone https://github.com/sjl/gundo.vim.git ./vim/bundle/gundo
-git clone https://github.com/tyrannicaltoucan/vim-deep-space.git ./vim/bundle/deep-space
-git clone https://github.com/tpope/vim-sensible.git ./vim/bundle/sensible
+git clone https://github.com/preservim/nerdtree.git .vim/bundle/nerdtree
+git clone https://github.com/ctrlpvim/ctrlp.vim.git .vim/bundle/ctrlp
+git clone https://github.com/sjl/gundo.vim.git .vim/bundle/gundo
+git clone https://github.com/tyrannicaltoucan/vim-deep-space.git .vim/bundle/deep-space
+git clone https://github.com/tpope/vim-sensible.git .vim/bundle/sensible
 ln -s ${DIR}/.vim ~/.vim
---
+++ jason.zsh-theme
@@ -0,0 +1,22 @@
+RANGE=%{$FG[214]%}
+YELLOW=%{$FG[220]%}
+GREEN=%{$FG[154]%}
+PURPLE=%{$FG[104]%}
+BLUE=%{$FG[075]%}
+PROMPT='%{$fg_bold[white]%}$ORANGE%n%{$fg[white]%} at $YELLOW%m%{$fg[white]%} in $GREEN%~%{$reset_color%}$(git_prompt_info) $BLUE$(git_prompt_status)%{$reset_color%}
+%(!.#.$) '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[white]%} on $PURPLE"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+
+ZSH_THEME_GIT_PROMPT_DIRTY=""
+ZSH_THEME_GIT_PROMPT_CLEAN=" (✔)"
+ZSH_THEME_GIT_PROMPT_ADDED="[+]"
+ZSH_THEME_GIT_PROMPT_MODIFIED="[*]"
+ZSH_THEME_GIT_PROMPT_DELETED="[x]"
+ZSH_THEME_GIT_PROMPT_RENAMED="[→]"
+ZSH_THEME_GIT_PROMPT_UNMERGED="[✂]"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="[✱]"
+ZSH_THEME_GIT_PROMPT_AHEAD="[↑]"
+ZSH_THEME_GIT_PROMPT_BEHIND="[↓]"
+
