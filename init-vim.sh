fc0194a8da93505d8495d786ebac0d0570757b9c
luca <luca1230@gmail.com>
2020-06-14 10:59:59 -0700

    added a method to only install vim plugins and vimrc

---
+++ init-vim.sh
@@ -0,0 +1,8 @@
+ln -s ${DIR}/.vimrc ~/.vimrc
+
+git clone https://github.com/preservim/nerdtree.git .vim/bundle/nerdtree
+git clone https://github.com/ctrlpvim/ctrlp.vim.git .vim/bundle/ctrlp
+git clone https://github.com/sjl/gundo.vim.git .vim/bundle/gundo
+git clone https://github.com/tyrannicaltoucan/vim-deep-space.git .vim/bundle/deep-space
+git clone https://github.com/tpope/vim-sensible.git .vim/bundle/sensible
+ln -s ${DIR}/.vim ~/.vim
