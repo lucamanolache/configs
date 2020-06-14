106ce482d6dc6970d102d7262e7bedf41fc78ecf
luca <luca1230@gmail.com>
2020-06-14 12:07:00 -0700

    added test docker file and changed ~ to $ HOME

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,19 @@
-FROM ubuntu
+FROM ubuntu AS ubuntu
 
-RUN apt-get update && apt-get install git
+RUN apt-get update
+RUN apt-get install -y --no-install-recommends ca-certificates git zsh vim
+
+ENV HOME /root
+ADD . ${HOME}/config
+
+RUN cd ${HOME}/config ; zsh -f ./init.sh
+
+
+FROM archlinux AS arch
+
+RUN pacman -S --noconfirm git zsh vim
+
+ENV HOME /root
+ADD . ${HOME}/config
+
+RUN cd ${HOME}/config ; zsh -f ./init.sh
--- init.sh
+++ init.sh
@@ -1,19 +1,19 @@
 DIR=$(pwd)
 
-ln -s ${DIR}/.vimrc ~/.vimrc
-ln -s ${DIR}/.zshrc ~/.zshrc
+ln -s "${DIR}"/.vimrc "${HOME}"/.vimrc
+ln -s "${DIR}"/.zshrc "${HOME}"/.zshrc
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 cp jason.zsh-theme .oh-my-zsh/themes/jason.zsh-theme
-ln -s ${DIR}/.oh-my-zsh ~/.oh-my-zsh
+ln -s "${DIR}"/.oh-my-zsh "${HOME}"/.oh-my-zsh
 
 git clone https://github.com/preservim/nerdtree.git .vim/bundle/nerdtree
 git clone https://github.com/ctrlpvim/ctrlp.vim.git .vim/bundle/ctrlp
 git clone https://github.com/sjl/gundo.vim.git .vim/bundle/gundo
 git clone https://github.com/tyrannicaltoucan/vim-deep-space.git .vim/bundle/deep-space
 git clone https://github.com/tpope/vim-sensible.git .vim/bundle/sensible
-ln -s ${DIR}/.vim ~/.vim
+ln -s "${DIR}"/.vim "${HOME}"/.vim
 
-source ~/.zshrc
+source "${HOME}"/.zshrc
