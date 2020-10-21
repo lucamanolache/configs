554c5bc13a9b52d42d92f0b427bdf5655db1e898
Luca Manolache <luca1230@gmail.com>
2020-10-21 15:44:08 -0700

    added more files for zsh

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias grep="grep -i"
+
+alias todo="todo.sh"
+alias ls="ls --color"
+alias ra=ranger
+alias vi="vim"
+
+alias emacs="emacsclient -create-frame --quiet --alternate-editor='vim'"
+
+alias zshconfig="vim ~/.zshrc"
